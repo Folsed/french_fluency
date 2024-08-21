8655378fe0307b2cf548fadaed71ef24e6535f46
Kyrylo <dexter37814@gmail.com>
2024-08-21 18:19:49 +0300

    feat: add 'link' tag to header Logo

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -3,14 +3,18 @@ import { LuUser2 } from 'react-icons/lu'
 import NavigationMenu from './NavigationMenu'
 import Links from './Links'
 import UserMenu from './UserMenu'
+import Link from 'next/link'
 
 const Header = () => {
     return (
         <section className='header'>
             <div className='lg:gap-22 fixed z-10 grid w-full grid-flow-col grid-cols-[1fr_auto_1fr] bg-[#000000b3] lg:px-12 lg:py-2'>
-                <div className='col-start-2 flex items-center lg:col-start-1'>
+                <Link
+                    className='col-start-2 flex items-center lg:col-start-1'
+                    href='/'
+                >
                     <Logo className='h-10 fill-[#FFD700] stroke-[#FFD700] pt-2' />
-                </div>
+                </Link>
                 <NavigationMenu />
                 <Links />
                 <UserMenu />
