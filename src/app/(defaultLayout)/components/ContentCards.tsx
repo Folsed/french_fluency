0660a4771d7cea4b531bece7b21f376c46a1e4fc
Kyrylo <dexter37814@gmail.com>
2024-09-11 17:56:36 +0300

    feat: add scroll to cards on button click

--- src/app/(defaultLayout)/components/ContentCards.tsx
+++ src/app/(defaultLayout)/components/ContentCards.tsx
@@ -3,21 +3,21 @@ import Link from 'next/link'
 
 const ContentCard = () => {
     return (
-        <div>
-            <div className='py-10 text-center'>
+        <div id='courses-grid'>
+            <div className='py-12 text-center lg:py-16'>
                 <h1 className='text-3xl font-bold lg:text-4xl xl:text-5xl'>
                     Мои курсы
                 </h1>
             </div>
-            <div className='relative grid grid-flow-row grid-cols-1 auto-rows-[minmax(300px,auto)] md:grid-cols-2'>
+            <div className='relative grid grid-flow-row auto-rows-[minmax(300px,auto)] grid-cols-1 md:grid-cols-2'>
                 {Array(5)
                     .fill(null)
                     .map((item, key) => (
                         <div
                             key={key}
-                            className='h-full aspect-square lg:aspect-[16/10]'
+                            className='aspect-square h-full lg:aspect-[16/10]'
                         >
-                            <button className='h-full relative w-full transition duration-300 flex justify-center items-center px-12 bg-[#000000c5] hover:bg-[#0000009b]'>
+                            <button className='relative flex h-full w-full items-center justify-center bg-[#000000c5] px-12 transition duration-300 hover:bg-[#0000009b]'>
                                 <Image
                                     className='z-[-1]'
                                     src='/123.jpg'
@@ -26,8 +26,8 @@ const ContentCard = () => {
                                     priority
                                     alt='Nature scene'
                                 />
-                                <div className='text-center z-[1] grid gap-6 '>
-                                    <h2 className='text-2xl font-bold text-font-hover truncate'>
+                                <div className='z-[1] grid gap-6 text-center'>
+                                    <h2 className='truncate text-2xl font-bold text-font-hover'>
                                         ЭКСКЛЮЗИВНЫЙ мастеркласс от
                                         сертифицированного экзаменатора
                                         DELF-DALF
