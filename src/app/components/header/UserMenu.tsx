4a868e91864bccf428859e797e5858de1e87ca64
Kyrylo <dexter37814@gmail.com>
2024-10-10 16:18:26 +0300

    feat: add token condition

--- src/app/components/header/UserMenu.tsx
+++ src/app/components/header/UserMenu.tsx
@@ -6,15 +6,25 @@ import Link from 'next/link'
 const UserMenu = () => {
     const { modalIs, setModalIs } = WebNavigation()
 
+    const token: string = ''
+
     return (
         <div className='__auth col-start-3 flex items-center justify-end'>
-            <Link
-                href={'/login'}
-                className='stroke-font p-2 duration-200 hover:stroke-font-hover'
-                title='Вход в профиль'
-            >
-                <LuUser2 size={32} stroke='inherit' />
-            </Link>
+            {token ? (
+                <span
+                    className='stroke-font p-2 duration-200 hover:stroke-font-hover text-white'
+                >
+                    Профиль
+                </span>
+            ) : (
+                <Link
+                    href={'/login'}
+                    className='stroke-font p-2 duration-200 hover:stroke-font-hover'
+                    title='Вход в профиль'
+                >
+                    <LuUser2 size={32} stroke='inherit' />
+                </Link>
+            )}
         </div>
     )
 }
