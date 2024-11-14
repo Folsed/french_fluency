2dc23fbc703bd969782430ab40c0306aca6ff032
Kyrylo <dexter37814@gmail.com>
2024-11-14 20:29:43 +0200

    styles: fix payment modal close canvas

--- src/components/modals/PaymentModal.tsx
+++ src/components/modals/PaymentModal.tsx
@@ -65,35 +65,33 @@ const PaymentModal = ({ amount }: { amount: number }) => {
 
     return (
         <div
-            className={`${modalIs === 'payment-modal' ? 'fixed' : 'fixed'} bottom-0 left-0 right-0 top-12 z-10 lg:top-[60px]`}
-            // onClick={() => setModalIs('')}
+            className={`${modalIs === 'payment-modal' ? 'fixed' : 'fixed'} inset-0 flex justify-center items-center`}
         >
-            <div className='fixed top-12 flex h-full w-full items-center justify-center overflow-y-auto bg-[#000000e1] lg:top-[60px]'>
-                <div className='relative mx-6 flex w-full max-w-xl flex-col gap-12 rounded-md bg-purple-100 p-8'>
-                    <h2 className='text-center text-3xl font-extrabold text-gray-800'>
-                        Оплата курса
-                    </h2>
+            <div
+                className='absolute inset-0 z-[2] flex bg-[#000000d1]'
+                onClick={() => setModalIs('')}
+            />
+            <div className='z-[3] relative mx-6 flex w-full max-w-xl flex-col gap-12 rounded-md bg-purple-100 p-8'>
+                <h2 className='text-center text-3xl font-extrabold text-gray-800'>
+                    Оплата курса
+                </h2>
 
-                    <form
-                        onSubmit={handleSubmit}
-                        className='flex flex-col gap-6'
+                <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
+                    {clientSecret && (
+                        <Suspense fallback={<span>Processing...</span>}>
+                            <PaymentElement />
+                        </Suspense>
+                    )}
+                    {errorMessage && <div>{errorMessage}</div>}
+
+                    <button
+                        disabled={!stripe || loading}
+                        type='submit'
+                        className='mb-2 me-2 inline-flex w-full items-center rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:bg-white dark:text-gray-900 dark:hover:bg-gray-200 dark:focus:ring-gray-800'
                     >
-                        {clientSecret && (
-                            <Suspense fallback={<span>Processing...</span>}>
-                                <PaymentElement />
-                            </Suspense>
-                        )}
-                        {errorMessage && <div>{errorMessage}</div>}
-
-                        <button
-                            disabled={!stripe || loading}
-                            type='submit'
-                            className='mb-2 me-2 inline-flex w-full items-center rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:bg-white dark:text-gray-900 dark:hover:bg-gray-200 dark:focus:ring-gray-800'
-                        >
-                            {loading ? 'Processing...' : `Pay ${amount}`}
-                        </button>
-                    </form>
-                </div>
+                        {loading ? 'Processing...' : `Pay ${amount}`}
+                    </button>
+                </form>
             </div>
         </div>
     )
