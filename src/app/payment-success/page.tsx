c7cdcadd055a380ccc1e566670dfb042b6c15381
Kyrylo <dexter37814@gmail.com>
2024-11-23 12:56:56 +0200

    fix: conflicts

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { Suspense } from 'react'
 import PaymentScreen from './components/PaymentScreen'
+import { Spinner } from '@/components/UI/Spinner'
 
 const PaymentSuccessPage = () => {
     return (
-        <Suspense>
+        <Suspense fallback={<Spinner/>}>
             <PaymentScreen />
         </Suspense>
     )
