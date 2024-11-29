2d19cb95fff645f8e280a9cc760b214c0b8d7607
Kyrylo <dexter37814@gmail.com>
2024-11-29 05:35:33 +0200

    fix: double incomplete stripe's intents

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -2,23 +2,29 @@ import { NextRequest, NextResponse } from 'next/server'
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 export const POST = async (request: NextRequest) => {
-    const url = new URL(request.url)
-    const courseName = url.searchParams.get('COURSE_NAME') || 'plug'
-
     try {
-        const { courseAmount } = await request.json()
+        const { courseAmount, courseName, confirmationTokenId } =
+            await request.json()
 
         const paymentIntent = await stripe.paymentIntents.create({
+            confirm: true,
             amount: courseAmount,
             currency: 'eur',
-            automatic_payment_methods: { enabled: true },
+            automatic_payment_methods: {
+                enabled: true,
+            },
             metadata: {
                 COURSE_NAME: courseName,
             },
             description: courseName,
+            confirmation_token: confirmationTokenId,
+            return_url: `${process.env.NEXT_PUBLIC_SERVER}/payment-success?COURSE_NAME=${encodeURIComponent(courseName)}&COURSE_AMOUNT=${courseAmount}`,
         })
 
-        return NextResponse.json({ clientSecret: paymentIntent.client_secret })
+        return NextResponse.json({
+            clientSecret: paymentIntent.client_secret,
+            status: paymentIntent.status,
+        })
     } catch (error) {
         console.error('Internal Error:', error)
 
